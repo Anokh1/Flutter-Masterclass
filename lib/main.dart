86bd4519d57b8738cb36603c078bf692dce8f899
Adrian001 <ooikeanhong168@gmail.com>
2024-07-14 17:54:46 +0800

    Update: add widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +18,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(title: Text("Your Heart")),
+        body: Center(
+          child: Container(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.circular(25)),
+              padding: EdgeInsets.all(25),
+              // child: Text("Anokh1"),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.white,
+                size: 99,
+              )),
+        ),
       ),
     );
   }
